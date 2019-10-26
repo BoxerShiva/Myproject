e6b2cdaa0e507f3d2fd99230ebbe2efda43bdf14
shiva.boxer1997@gmail.com <shiva.boxer1997@gmail.com>
2019-10-26 21:53:17 +0530

    Updates

---
+++ precache-manifest.24d7829f83d0aff29aa769019bfff433.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745a9c786bf14556d5456b3d75d76531",
+    "url": "/Myproject/index.html"
+  },
+  {
+    "revision": "e6444e978c224aec6ade",
+    "url": "/Myproject/static/css/main.204acae6.chunk.css"
+  },
+  {
+    "revision": "b0d2be70b3d6c2f7e7ab",
+    "url": "/Myproject/static/js/2.081eda3b.chunk.js"
+  },
+  {
+    "revision": "e6444e978c224aec6ade",
+    "url": "/Myproject/static/js/main.714d453a.chunk.js"
+  },
+  {
+    "revision": "f414d776953245535a9c",
+    "url": "/Myproject/static/js/runtime~main.f72a2ce4.js"
+  }
+]);
